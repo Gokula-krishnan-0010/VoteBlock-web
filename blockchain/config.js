1b6c89d05262ba9e47aa56f7a9c66a311cd34fc2
KUMARAGURU-V-S <kumaraguruprivate@gmail.com>
2026-02-12 22:18:42 +0530

    smart contract tested with add candidate and vote

---
+++ blockchain/config.js
@@ -0,0 +1,5 @@
+import votingArtifact from "./artifacts/contracts/VotingV1.sol/VotingV1.json";
+
+// The address you got from your successful deployment
+export const VOTING_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const VOTING_ABI = votingArtifact.abi;
